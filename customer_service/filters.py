739a1eb4654f706d4c50de4849fcb1f43845a5fc
Hryhorii Mosia <mosia.dev@gmail.com>
2020-11-21 04:53:10 +0200

    Added new fields to InsurancePolicyFilter filter and customizing them

--- customer_service/filters.py
+++ customer_service/filters.py
@@ -1,11 +1,22 @@
 import django_filters
 
-from .models import InsurancePolicy
+from .models import InsurancePolicy, Customer
 
 
 class InsurancePolicyFilter(django_filters.FilterSet):
+    customer = django_filters.CharFilter(field_name='customer',
+                                         method='filter_customer')
+
+    car = django_filters.CharFilter(field_name='car',
+                                    method='filter_car')
+
+    def filter_customer(self, queryset, name, value):
+        return queryset.filter(customer__name__icontains=value)
+
+    def filter_car(self, queryset, name, value):
+        return queryset.filter(car__registration_number__icontains=value)
+
     class Meta:
         model = InsurancePolicy
         fields = ['number', 'registration_date', 'begin_date', 'end_date',
-                  'insurance_code']
-
+                  'customer', 'car', 'insurance_code']
